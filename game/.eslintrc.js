e9c4ad5b19c0d87027347ba3e4fd398cb12ae623
jonas3688 <joaocapozzi60@gmail.com>
2026-06-10 00:11:52 -0300

    fix: corrige syntax error no mk.js e configura jest no eslint

---
+++ game/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": "eslint:recommended",
+  "globals": {
+    "io": "readonly",
+    "Movement": "readonly"
+  },
+  "rules": {
+    "no-unused-vars": "warn",
+    "no-console": "off",
+    "no-extra-semi": "off"
+  }
+};
